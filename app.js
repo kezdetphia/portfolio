e8ceb8ca9f49863d903c09fae6398baaac074abb
kezdetphia <fehermark88@gmail.com>
2023-06-05 14:15:38 +0200

    Menu bar set up, Landing/intro page set up. Menu bar texts are made to anchor tags too.

---
+++ app.js
@@ -0,0 +1,27 @@
+const intro2 = "I'm a visionary software engineer, crafting cutting-edge solutions and shaping the digital future with my code."
+
+
+
+
+
+
+
+
+
+
+
+$(()=>{
+  //menubar 
+  const $icon= $('<img>').attr({'src' : '/Users/WorkSpace/code/portfolio/utils/atom.png', 'id' : 'icon'} ).appendTo('.icon-name-div').wrap('<a href="https://www.google.com"></a>');
+  const $name = $('<h4>').text('mark feher').attr('id', 'name').appendTo('.icon-name-div').wrap('<a href="https://www.google.com"></a>');
+  const $home = $('<h4>').text('home').attr('class', 'menu-items').appendTo('.menu-options').wrap('<a href="https://www.google.com"></a>');
+  const $about = $('<h4>').text('about').attr('class', 'menu-items').appendTo('.menu-options').wrap('<a href="https://www.google.com"></a>');
+  const $projects = $('<h4>').text('projects').attr('class', 'menu-items').appendTo('.menu-options').wrap('<a href="https://www.google.com"></a>');
+  const $contact = $('<h4>').text('contact').attr('class', 'menu-items').appendTo('.menu-options').wrap('<a href="https://www.google.com"></a>');
+
+  //main
+  const $intro = $('<h1>').text("hi, i'm mark feher").attr('id', 'intro').appendTo('.welcome-div')
+  const $intro2 = $('<h3>').text(intro2).attr('id', 'intro2').appendTo('.welcome-div');
+  const $projbtn = $('<button>').text('projects').attr('id', 'projbtn').appendTo('.welcome-div')
+
+})
