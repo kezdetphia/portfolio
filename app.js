4381b63c4cbfbfb77f7f660a463c760562ea0b60
kezdetphia <fehermark88@gmail.com>
2023-06-08 12:29:40 +0200

    All the features work as expected, only the change theme button should be placed elswhere from the bottom of ths screen, otherwise its kinda done

--- app.js
+++ app.js
@@ -15,27 +15,27 @@ const picList = [
 
 const loadLayOut=()=>{
   //menubar 
-  const $icon= $('<img>').attr({'src' : '/Users/WorkSpace/code/portfolio/utils/atom.png', 'id' : 'icon'} ).appendTo('.icon-name-div').wrap('<a href="https://www.google.com"></a>');
-  const $name = $('<h4>').text('mark feher').attr('id', 'name').appendTo('.icon-name-div').wrap('<a href="https://www.google.com"></a>');
-  //  <button class="invert-colors">change theme</button>
-  // const $invertButtton = $('<button>').attr({"inner.Text": 'change theme', 'class':'invert-colors',}).appendTo('.menu-options')
-  const $home = $('<h4>').text('home').attr('class', 'menu-items').appendTo('.menu-options').wrap('<a href="https://www.google.com"></a>');
-  // const $invertButtton = $('<h4>').innerText('change thene').attr({"inner.Text": 'change theme', 'class':'invert-colors',}).appendTo('.menu-options').wrap('<a href="#"></a>');
-  const $about = $('<h4>').text('about').attr('class', 'menu-items').appendTo('.menu-options').wrap('<a href="https://www.google.com"></a>');
-  const $projects = $('<h4>').text('projects').attr('class', 'menu-items').appendTo('.menu-options').wrap('<a href="https://www.google.com"></a>');
-  const $contact = $('<h4>').text('contact').attr('class', 'menu-items', ).appendTo('.menu-options').wrap('<a href="https://www.google.com"></a>');
+  const $icon= $('<img>').attr({'src' : '/Users/WorkSpace/code/portfolio/utils/atom.png', 'id' : 'icon'} ).appendTo('.icon-name-div').click(() => location.reload());
+  const $name= $('<h4>').text('mark feher').attr({'id': 'name',}).appendTo('.icon-name-div').wrap('<a href="#"></a>').parent().on('click', () => window.location.reload());
+  const $home = $('<h4>').text('home').attr('class', 'menu-items').appendTo('.menu-options').wrap('<a href="#"></a>').parent().on('click', () => window.location.reload());
+  const $about = $('<h4>').text('about').attr('class', 'menu-items').appendTo('.menu-options').wrap('<a class="scroll-link" href=".about-me" ></a>');
+  const $projects = $('<h4>').text('projects').attr('class', 'menu-items').appendTo('.menu-options').wrap('<a class="scroll-link" href=".projects"></a>');
+  const $contact = $('<h4>').text('contact').attr({'class': 'menu-items', 'id': 'openModal'} ).appendTo('.menu-options').wrap('<a href="#"></a>');
 
   //main
   const $intro = $('<h1>').text("hi, i'm mark feher").attr('id', 'intro').appendTo('.introduction')
   const $intro2 = $('<h3>').text(intro2).attr('id', 'intro2').appendTo('.introduction');
-  // const $projbtn = $('<button>').text('projects').attr('id', 'projbtn').appendTo('.introduction')
   const $projbtn = $('<button>').text('projects').attr({'id':'openModal2', 'class':"modal-buttons"}).appendTo('.introduction')
 
   //main//links
   const $linkedInImg = $('<img>').attr({'src' : 'utils/link.png' , 'id' : 'linkedin-icon' ,'class': 'sm-links', } ).appendTo('.links').wrap('<a href="https://www.linkedin.com/in/mark-feher-3484a2182/" target="_blank" ></a>');
+  $linkedInImg.clone().appendTo('.footer-right-icon').wrap('<a href="https://www.linkedin.com/in/mark-feher-3484a2182/" target="_blank" ></a>');
   const $gitHubImg = $('<img>').attr({'src' : 'utils/github.png' , 'id' : 'github-icon', 'class': 'sm-links' } ).appendTo('.links').wrap('<a href="https://github.com/kezdetphia" target="_blank"></a>');
+  $gitHubImg.clone().appendTo('.footer-right-icon').wrap('<a href="https://github.com/kezdetphia" target="_blank"></a>');
   const $instaImg = $('<img>').attr({'src' : 'utils/insta.png' , 'id' : 'insta-icon', 'class': 'sm-links' } ).appendTo('.links').wrap('<a href="https://github.com/kezdetphia" target="_blank"></a>');
+  $instaImg.clone().appendTo('.footer-right-icon').wrap('<a href="https://github.com/kezdetphia" target="_blank"></a>');
   const $faceImg = $('<img>').attr({'src' : 'utils/facebook(1).png' , 'id' : 'face-icon', 'class': 'sm-links' } ).appendTo('.links').wrap('<a href="https://github.com/kezdetphia" target="_blank"></a>');
+  $faceImg.clone().appendTo('.footer-right-icon').wrap('<a href="https://github.com/kezdetphia" target="_blank"></a>');
 
   //aboutMe
   const $aboutMe = $('<h1>').text('about me.').appendTo('.about-me-intro')
@@ -62,6 +62,7 @@ const changeColors=()=>{
 
 
 $(()=>{
+
 loadLayOut()
 
 //pup up window for contact details when button is pressed
@@ -91,12 +92,16 @@ $openBtn2.on('click', openModal2);
 $closeBtn2.on('click', closeModal2);
 
 
-   
-//iterates through the pictures list and displaying then in the slider
-picList.forEach((picLink)=>{
-  $carouselInside = $('.carousel-inner')
-  $('<img>').attr({'src': picLink, id: 'carousel-img'}).appendTo($carouselInside)
-})
+//this is a pretty primitive way to make the slide longer. The ideal idea would be if
+//the pic array would load again or extend it by the array items once it reaches the end
+//also due to this solution when i start with the previous button it shows blank
+for (let i=0; i<10; i++){
+  picList.forEach((picLink)=>{
+    $carouselInside = $('.carousel-inner')
+    $('<img>').attr({'src': picLink, id: 'carousel-img'}).appendTo($carouselInside)
+  })
+}
+
 
 //projects slide. When button is clicked it checks the width of the first image and 'transforms' the image on the x axis as many pixels
 //as the current first images width is in pixels 
@@ -118,8 +123,19 @@ $(".carousel-prev").click(function(e) {
 $('.invert-colors').on('click', changeColors)
 
 
+$('a.scroll-link').click(function(e) {
+  e.preventDefault();
+  var targetDiv = $($(this).attr('href'));
+  var offset = targetDiv.offset().top;
+  $('html, body').animate({ scrollTop: offset }, 500);
+});
 
-
+$('a.projects').click(function(e) {
+  e.preventDefault();
+  var targetDiv = $($(this).attr('href'));
+  var offset = targetDiv.offset().top;
+  $('html, body').animate({ scrollTop: offset }, 500);
+});
 
 
 
